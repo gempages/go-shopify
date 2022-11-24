b0e4e5ea509e794343025989eb6dfa85d179f386
leon.vu <leon.vu@gempages.help>
2022-11-24 14:01:40 +0700

    Update editor config

--- options.go
+++ options.go
@@ -1,9 +1,9 @@
 package goshopify
 
 import (
-    "context"
-    "fmt"
-    "net/http"
+	"context"
+	"fmt"
+	"net/http"
 )
 
 // Option is used to configure client with options
@@ -11,38 +11,38 @@ type Option func(c *Client)
 
 // WithVersion optionally sets the api-version if the passed string is valid
 func WithContext(ctx context.Context) Option {
-    return func(c *Client) {
-        c.ctx = ctx
-    }
+	return func(c *Client) {
+		c.ctx = ctx
+	}
 }
 
 // WithVersion optionally sets the api-version if the passed string is valid
 func WithVersion(apiVersion string) Option {
-    return func(c *Client) {
-        pathPrefix := defaultApiPathPrefix
-        if len(apiVersion) > 0 && (apiVersionRegex.MatchString(apiVersion) || apiVersion == UnstableApiVersion) {
-            pathPrefix = fmt.Sprintf("admin/api/%s", apiVersion)
-        }
-        c.apiVersion = apiVersion
-        c.pathPrefix = pathPrefix
-    }
+	return func(c *Client) {
+		pathPrefix := defaultApiPathPrefix
+		if len(apiVersion) > 0 && (apiVersionRegex.MatchString(apiVersion) || apiVersion == UnstableApiVersion) {
+			pathPrefix = fmt.Sprintf("admin/api/%s", apiVersion)
+		}
+		c.apiVersion = apiVersion
+		c.pathPrefix = pathPrefix
+	}
 }
 
 func WithRetry(retries int) Option {
-    return func(c *Client) {
-        c.retries = retries
-    }
+	return func(c *Client) {
+		c.retries = retries
+	}
 }
 
 func WithLogger(logger LeveledLoggerInterface) Option {
-    return func(c *Client) {
-        c.log = logger
-    }
+	return func(c *Client) {
+		c.log = logger
+	}
 }
 
 // WithHTTPClient is used to set a custom http client
 func WithHTTPClient(client *http.Client) Option {
-    return func(c *Client) {
-        c.Client = client
-    }
+	return func(c *Client) {
+		c.Client = client
+	}
 }
